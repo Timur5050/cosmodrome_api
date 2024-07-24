17d78192d03f5aa8b96297b46597d78522ff5c68
pro <pro@pros-MBP.bbrouter>
2024-07-24 18:02:33 +0300

    created routers for all models

--- cosmodrome/urls.py
+++ cosmodrome/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/cosmodrome/', include("hangar.urls", namespace="cosmodrome")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
